465d5cd260169c6e501c54acaf23ad092796a601
MayconFrr <32937928+MayconFrr@users.noreply.github.com>
2022-03-19 13:46:00 -0300

    Add fraud check service, returning mocked response

---
+++ fraud/src/main/java/io/github/mayconfrr/fraud/FraudCheckService.java
@@ -0,0 +1,18 @@
+package io.github.mayconfrr.fraud;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public record FraudCheckService(FraudCheckHistoryRepository fraudCheckHistoryRepository) {
+    public boolean isFraudulentCustomer(Long customerId) {
+        FraudCheckHistory fraudCheckHistory = FraudCheckHistory.builder()
+                .customerId(customerId)
+                .isFraudster(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+        fraudCheckHistoryRepository.save(fraudCheckHistory);
+        return false;
+    }
+}
