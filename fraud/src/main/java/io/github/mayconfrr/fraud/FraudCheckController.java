2c5a4bf155055cb116cfb3e50d83e9f2e1bb0800
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-03-19 14:24:43 -0300

    Add missing logging

--- fraud/src/main/java/io/github/mayconfrr/fraud/FraudCheckController.java
+++ fraud/src/main/java/io/github/mayconfrr/fraud/FraudCheckController.java
@@ -1,13 +1,16 @@
 package io.github.mayconfrr.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public record FraudCheckController(FraudCheckService fraudCheckService) {
 
     @GetMapping("{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable Long customerId) {
+        log.info("new fraud check request for customer {}", customerId);
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
