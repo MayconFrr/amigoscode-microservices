feca17a15c9637e49f67618558baf16e0e4a8d97
MayconFrr <32937928+MayconFrr@users.noreply.github.com>
2022-03-20 19:42:24 -0300

    Change customer microservice to publish to notification queue

--- customer/src/main/java/io/github/mayconfrr/customer/CustomerApplication.java
+++ customer/src/main/java/io/github/mayconfrr/customer/CustomerApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "io.github.mayconfrr.customer",
+                "io.github.mayconfrr.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "io.github.mayconfrr.clients")
 public class CustomerApplication {
