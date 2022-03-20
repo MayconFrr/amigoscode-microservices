631c5f30eab3976d8874129e1346148485f009d7
MayconFrr <32937928+MayconFrr@users.noreply.github.com>
2022-03-19 21:07:38 -0300

    Add communication between customer and fraud microservices through service discovery

--- customer/src/main/java/io/github/mayconfrr/customer/CustomerService.java
+++ customer/src/main/java/io/github/mayconfrr/customer/CustomerService.java
@@ -1,9 +1,12 @@
 package io.github.mayconfrr.customer;
 
+import io.github.mayconfrr.clients.fraud.FraudCheckResponse;
+import io.github.mayconfrr.clients.fraud.FraudClient;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, FraudClient fraudClient) {
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstname(request.firstname())
@@ -12,8 +15,15 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
 
         // @TODO: check if email valid
-        // @TODO: check if email not token
+        // @TODO: check if email not taken
 
         customerRepository.save(customer);
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new CustomerFraudsterException("Customer " + customer.getFirstname() + " " + customer.getLastname() + " is fraudulent");
+        }
+
+        // @TODO: send notification
     }
 }
